acb70a800fe087d02db2a545749d347b7939877b
bensaei <behnam.saeidi@gmail.com>
2019-01-25 22:49:48 -0800

    model-2 trained with improved accuracy

--- Assignments/Assignment 1/keypoint_description/keypoint_description/descriptor.py
+++ Assignments/Assignment 1/keypoint_description/keypoint_description/descriptor.py
@@ -21,11 +21,12 @@ import PIL
 import torch.nn as nn
 import torch.nn.functional as F
 from Utils import L2Norm, cv2_scale, np_reshape
-class DesNet(nn.Module):
+
+class DesNet_1(nn.Module):
     """DesdNet model definition
     """
     def __init__(self):
-        super(DesNet, self).__init__()
+        super(DesNet_1, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias = False),
             nn.BatchNorm2d(32, affine=False),
@@ -51,6 +52,65 @@ class DesNet(nn.Module):
         x = x_features.view(x_features.size(0), -1)
         return L2Norm()(x)
 
+class DesNet_2(nn.Module):
+    """DesdNet model definition
+    """
+    def __init__(self):
+        super(DesNet_2, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias = True),
+            nn.BatchNorm2d(32, affine=False),
+            nn.PReLU(),
+            nn.Conv2d(32, 128, kernel_size=3, stride=2, padding=1, bias = True),
+            nn.BatchNorm2d(128, affine=False),
+            nn.PReLU(),
+            nn.Dropout(0.3),
+            nn.Conv2d(128, 128, kernel_size=8, bias = False),
+            nn.BatchNorm2d(128, affine=False),
+        )
+        self.features.apply(weights_init)
+        return
+    
+    def input_norm(self,x):
+        flat = x.view(x.size(0), -1)
+        mp = torch.mean(flat, dim=1)
+        sp = torch.std(flat, dim=1) + 1e-7
+        return (x - mp.detach().unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand_as(x)) / sp.detach().unsqueeze(-1).unsqueeze(-1).unsqueeze(1).expand_as(x)
+    
+    def forward(self, input):
+        x_features = self.features(self.input_norm(input))
+        x = x_features.view(x_features.size(0), -1)
+        return L2Norm()(x)
+
+class DesNet_3(nn.Module):
+    """DesdNet model definition
+    """
+    def __init__(self):
+        super(DesNet_2, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias = False),
+            nn.BatchNorm2d(32, affine=False),
+            nn.PReLU(),
+            nn.Conv2d(32, 128, kernel_size=3, stride=2, padding=1, bias = False),
+            nn.BatchNorm2d(128, affine=False),
+            nn.PReLU(),
+            nn.Dropout(0.3),
+            nn.Conv2d(128, 128, kernel_size=8, bias = False),
+            nn.BatchNorm2d(128, affine=False),
+        )
+        self.features.apply(weights_init)
+        return
+    
+    def input_norm(self,x):
+        flat = x.view(x.size(0), -1)
+        mp = torch.mean(flat, dim=1)
+        sp = torch.std(flat, dim=1) + 1e-7
+        return (x - mp.detach().unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand_as(x)) / sp.detach().unsqueeze(-1).unsqueeze(-1).unsqueeze(1).expand_as(x)
+    
+    def forward(self, input):
+        x_features = self.features(self.input_norm(input))
+        x = x_features.view(x_features.size(0), -1)
+        return L2Norm()(x)
 def weights_init(m):
     if isinstance(m, nn.Conv2d):
         nn.init.orthogonal_(m.weight.data, gain=0.6)
